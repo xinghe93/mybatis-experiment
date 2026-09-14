2a212e558321274ed157aee21df8a6fb8ce40b23
xinghe <111168693+xinghe93@users.noreply.github.com>
2026-09-14 17:02:23 +0800

    初始化项目

---
+++ src/main/java/com/xinghe/MybatisExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.xinghe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisExperimentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisExperimentApplication.class, args);
+    }
+
+}
